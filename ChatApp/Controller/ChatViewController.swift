f464378fa339470732693f607beca0ec9dc5e2f4
Kamil Çal <kamilcaal@gmail.com>
2022-12-17 17:01:18 +0300

    Fixed: the fact that the topmost cells do not appear when the keyboard is scrolled.

--- ChatApp/Controller/ChatViewController.swift
+++ ChatApp/Controller/ChatViewController.swift
@@ -37,7 +37,30 @@ class ChatViewController: UIViewController {
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
 
-    }
+        let notificationCenter = NotificationCenter.default
+
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+}
+
+    @objc func adjustForKeyboard(notification: Notification) {
+
+            guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+            let keyboardScreenEndFrame = keyboardValue.cgRectValue
+
+            let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+            if notification.name == UIResponder.keyboardWillHideNotification {
+                tableView.contentInset = .zero
+            } else {
+                tableView.contentInset = UIEdgeInsets(top: 300, left: 0, bottom: keyboardViewEndFrame
+                    .height > 10 ? 10 : 10, right: 0)
+            }
+            tableView.scrollIndicatorInsets = tableView.contentInset
+        }
+    
     
     func loadMessage() {
         
